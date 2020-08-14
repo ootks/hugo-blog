0ed24ef4340c3c9a9c71efc112d4db9b17f34264
kshu <shu.kevins@gmail.com>
2020-08-13 23:57:43 -0400

    Added the CNAME for DNS redirects.

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,7 @@
 # Deploys the blog to the directory in the second argument using the base url
 # given in the first argument
 mv config.toml .hidden
-sed s/baseurl\ =\ \".*\"/baseurl\ =\ \"$1\"/ .hidden >> config.toml
+REPLACE=$(printf '%s\n' "$1" | sed -e 's/[\/&]/\\&/g')
+sed s/baseurl\ =\ \".*\"/baseurl\ =\ \"$REPLACE\"/ .hidden >> config.toml
 hugo -D -d $2
 mv .hidden config.toml
